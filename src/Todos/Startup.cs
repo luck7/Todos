10961d67caf357efff9610440413a4cdc627db8c
Demis Bellot <demis.bellot@gmail.com>
2016-09-21 17:45:11 -0400

    Add .NET Core Todos Web App

--- src/Todos/Startup.cs
+++ src/Todos/Startup.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using System.Reflection;
+using Funq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using ServiceStack;
 using ServiceStack.Redis;
-using Funq;
 using ServiceStack.Text;
 
-//The entire C# source code for the ServiceStack + Redis TODO REST backend. There is no other .cs :)
 namespace Todos
 {
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseServiceStack(new AppHost());
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+
+
     // Define your ServiceStack web service request (i.e. Request DTO).
     [Route("/todos")]
     [Route("/todos/{Id}")]
@@ -35,13 +67,13 @@ namespace Todos
         public Todo Post(Todo todo)
         {
             var redis = Redis.As<Todo>();
-            
+
             //Get next id for new todo
-            if (todo.Id == default(long)) 
+            if (todo.Id == default(long))
                 todo.Id = redis.GetNextSequence();
-            
+
             redis.Store(todo);
-            
+
             return todo;
         }
 
@@ -62,7 +94,7 @@ namespace Todos
     public class AppHost : AppHostBase
     {
         // Initializes your ServiceStack App Instance, with the specified assembly containing the services.
-        public AppHost() : base("Backbone.js TODO", typeof(TodoService).Assembly) { }
+        public AppHost() : base("Backbone.js TODO", typeof(TodoService).GetTypeInfo().Assembly) { }
 
         // Configure the container with the necessary routes for your ServiceStack application.
         public override void Configure(Container container)
@@ -72,15 +104,6 @@ namespace Todos
 
             //Register Redis factory in Funq IoC. The default port for Redis is 6379.
             container.Register<IRedisClientsManager>(new BasicRedisClientManager("localhost:6379"));
-        }
-    }
-
-    public class Global : System.Web.HttpApplication
-    {
-        protected void Application_Start(object sender, EventArgs e)
-        {
-            //Initialize your ServiceStack AppHost
-            new AppHost().Init();
         }
     }
 }
